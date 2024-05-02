128c7d7cf08ab7994f51dd4b29a9050a931853df
Hannah Overton <hannahoverton99@gmail.com>
2024-05-02 15:01:14 -0500

    studio react 1

--- part1/exercises/src/components/BookList.jsx
+++ part1/exercises/src/components/BookList.jsx
@@ -1,15 +1,15 @@
 export default function BookList() {
-   let pageTitle = "shrug";
-   let book1 = "google.com";
-   let book2 = "";
-   let book3 = "";
+   let bookList = "My Favorite Books";
+   let book1 = "https://luckys.ca/cdn/shop/files/howl.jpg?v=1697845743&width=1946";
+   let book2 = "https://images.pangobooks.com/images/a784fb37-1238-471e-b05f-0ab97fdeaf41?width=800&quality=85&crop=1%3A1";
+   let book3 = "https://www.harpercollins.com/cdn/shop/files/9780063040502_1200x1200.jpg?v=1713018133";
 
    return (
       <div>
-         <h3>{pageTitle}</h3>
-         <img src={book1} alt="Nothing here!" />
-         <img src={book2} alt="Nothing here!" />
-         <img src={book3} alt="Nothing here!" />
+         <h3>{bookList}</h3>
+         <img src={book1} alt="Howl's Moving Castle" />
+         <img src={book2} alt="Tiger Lily" />
+         <img src={book3} alt="Sabriel" />
       </div>      
    );
 }
